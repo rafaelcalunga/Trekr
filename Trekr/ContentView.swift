096722510afd0cfa644b17d6be0c80fd60bc90d5
Rafael Calunga <rafaelcalunga@gmail.com>
2020-12-22 13:19:48 -0500

    Building some initial layout

--- Trekr/ContentView.swift
+++ Trekr/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            Image("highlands")
+                .resizable()
+                .scaledToFit()
+            
+            Text("The Highlands")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+            
+            Text("United Kingdom")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text("The Highlands is a historic region of Scotland. Culturally, the Highlands and the Lowlands diverged from the later Middle Ages into the modern period, when Lowland Scots replaced Scottish Gaelic throughout most of the Lowlands. The term is also used for the area north and west of the Highland Boundary Fault, although the exact boundaries are not clearly defined, particularly to the east. The Great Glen divides the Grampian Mountains to the southeast from the Northwest Highlands. The Scottish Gaelic name of A' Ghàidhealtachd literally means \"the place of the Gaels\" and traditionally, from a Gaelic-speaking point of view, includes both the Western Isles and the Highlands.")
+                .padding(.horizontal)
+            
+            Text("Did you know?")
+                .font(.title3)
+                .bold()
+                .padding(.top)
+            
+            Text("The Highlands is a historic region of Scotland. Culturally, the Highlands and the Lowlands diverged from the later Middle Ages into the modern period, when Lowland Scots replaced Scottish Gaelic throughout most of the Lowlands. The term is also used for the area north and west of the Highland Boundary Fault, although the exact boundaries are not clearly defined, particularly to the east. The Great Glen divides the Grampian Mountains to the southeast from the Northwest Highlands. The Scottish Gaelic name of A' Ghàidhealtachd literally means \"the place of the Gaels\" and traditionally, from a Gaelic-speaking point of view, includes both the Western Isles and the Highlands.")
+                .padding(.horizontal)
+        }
+        .navigationTitle("Discover")
     }
 }
 
