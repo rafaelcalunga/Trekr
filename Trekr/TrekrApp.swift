4534431ec390c6831dc6e0c7738dadf33974d563
Rafael Calunga <rafaelcalunga@gmail.com>
2020-12-22 12:19:42 -0500

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Rafael Calunga on 2020-12-22.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
